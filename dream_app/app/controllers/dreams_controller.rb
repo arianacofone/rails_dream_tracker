d992bb46b1a77626a25b3c34ffa41ab42c638456
arianadziedzic <ariana.dziedzic@gmail.com>
2016-10-25 14:44:50 -0400

    create and configure dreams controller

---
+++ dream_app/app/controllers/dreams_controller.rb
@@ -0,0 +1,50 @@
+class DreamsController < ApplicationController
+  before_action :get_dreams, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @dreams = Dream.all
+  end
+
+  def show
+  end
+
+  def new
+    @dream = Dream.new
+  end
+
+  def create
+    dream = Dream.new(post_params)
+    if dream.save
+      redirect_to dream
+    else
+      redirect_to new_dream_path
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @dream.update(dream_params)
+      redirect_to @dream
+    else
+      redirect_to edit_dream_path(@dream)
+    end
+  end
+
+  def destroy
+    if @dream.destroy
+      redirect_to dreams_path
+    end
+  end
+
+  private
+
+  def dream_params
+    params.require(:dream).permit(:title, :dream)
+  end
+
+  def get_dream
+    @dream ||= Dream.find(params[:id])
+  end
+end
